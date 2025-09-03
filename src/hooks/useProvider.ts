561b43422fd88925fb01da81ac31aafd99e4bf4b
philix27 <philixbob@gmail.com>
2025-09-03 03:36:46 +0100

    use dynamic providers

--- src/hooks/useProvider.ts
+++ src/hooks/useProvider.ts
@@ -1,14 +1,19 @@
 import { ethers } from 'ethers'
 import { useMemo } from 'react';
+import { base, celo } from 'viem/chains';
 
 
 export const useProvider = () => {
   // Memoized provider
-  const provider = useMemo(
-    () => new ethers.JsonRpcProvider("https://forno.celo.org"),
+  const providerCelo = useMemo(
+    () => new ethers.JsonRpcProvider(celo.rpcUrls.default.http[0]),
+    []
+  );
+  const providerBase = useMemo(
+    () => new ethers.JsonRpcProvider(base.rpcUrls.default.http[0]),
     []
   );
 
   // const provider = new ethers.BrowserProvider(window.ethereum)
-  return provider
+  return { celo: providerCelo, base: providerBase }
 }
--- src/hooks/useSend.ts
+++ src/hooks/useSend.ts
@@ -1,10 +1,12 @@
 import { ethers, } from 'ethers'
-// import { toast } from 'sonner'
-import { useSwitchChain } from 'wagmi'
-// import * as divvi from '@divvi/referral-sdk'
-// import { appAddresses } from '../lib/const'
-// import { logger } from '../lib/utils'
+import { toast } from 'sonner'
+import { useAccount, useSwitchChain } from 'wagmi'
+import * as divvi from '@divvi/referral-sdk'
+import { appAddresses } from '../lib/const'
+import { logger } from '../lib/utils'
 import { IPayWith } from '../features/pay/tokens'
+import { useProvider } from './useProvider'
+import { ChainName } from '../features/pay/chains'
 
 
 export interface ISendTxnError {
@@ -63,88 +65,88 @@ interface Param {
 }
 
 
-// const ERC20_ABI = ['function transfer(address recipient, uint256 amount) public returns (bool)']
+const ERC20_ABI = ['function transfer(address recipient, uint256 amount) public returns (bool)']
 
 
 export function useSendToken() {
-  // const { address, } = useAccount();
-  const provider = new ethers.BrowserProvider(window.ethereum)
+  const { address, } = useAccount();
+  const provider = useProvider()
 
   const {
     switchChain,
   } = useSwitchChain();
 
   const sendErc20 = async (props: { recipient: string; amount: string; payWith: IPayWith }) => {
-    // const signer = await provider.getSigner()
-
-
-    // if (!signer) {
-    //   toast.error('Please connect your wallet')
-    //   throw new Error('Signer needed')
-    // }
-
     const chain = props.payWith.chain
+    const activeProvider = chain.name === ChainName.Base ? provider.base : provider.celo;
+
+
+    const signer = await activeProvider.getSigner()
+
+
+    if (!signer) {
+      toast.error('Please connect your wallet')
+      throw new Error('Signer needed')
+    }
+
+
     switchChain({ chainId: chain.chainId });
 
 
-    // const token = props.payWith.token
-    // const contract = new ethers.Contract(
-    //   token.address,
-    //   ERC20_ABI,
-    //   signer
-    // )
-    console.log( provider)
+    const token = props.payWith.token
+    const contract = new ethers.Contract(
+      token.address,
+      ERC20_ABI,
+      signer
+    )
 
     // // ! Start Divvi
-    // const transferData = contract.interface.encodeFunctionData("transfer", [
-    //   props.recipient,
-    //   ethers.parseUnits(props.amount, token.decimal),
-    // ]);
+    const transferData = contract.interface.encodeFunctionData("transfer", [
+      props.recipient,
+      ethers.parseUnits(props.amount, token.decimal),
+    ]);
 
-    // // Generate Divvi referral tag
-    // const dataSuffix = divvi.getReferralTag({
-    //   user: address!, // The user address making the transaction
-    //   consumer: appAddresses.divvi as `0x${string}`, // Your Divvi consumer address
-    // })
+    // Generate Divvi referral tag
+    const dataSuffix = divvi.getReferralTag({
+      user: address!, // The user address making the transaction
+      consumer: appAddresses.divvi as `0x${string}`, // Your Divvi consumer address
+    })
 
-    // // Append Divvi suffix to transaction data
-    // let dataWithSuffix = transferData;
-    // //  const txData = transferData + dataSuffix;
-    // // const txData = transferData + ethers.hexlify(ethers.toUtf8Bytes(dataSuffix)).slice(2);
+    // Append Divvi suffix to transaction data
+    let dataWithSuffix = transferData;
+    //  const txData = transferData + dataSuffix;
+    // const txData = transferData + ethers.hexlify(ethers.toUtf8Bytes(dataSuffix)).slice(2);
 
-    // if (dataSuffix && dataSuffix.startsWith('0x')) {
-    //   // Remove '0x' prefix from suffix before concatenating
-    //   dataWithSuffix = transferData + dataSuffix.slice(2);
-    // } else if (dataSuffix) {
-    //   dataWithSuffix = transferData + dataSuffix;
-    // }
+    if (dataSuffix && dataSuffix.startsWith('0x')) {
+      // Remove '0x' prefix from suffix before concatenating
+      dataWithSuffix = transferData + dataSuffix.slice(2);
+    } else if (dataSuffix) {
+      dataWithSuffix = transferData + dataSuffix;
+    }
 
-    // // // Send transaction
-    // const txn = await signer.sendTransaction({
-    //   to: token.address,
-    //   data: dataWithSuffix,
-    // });
+    // // Send transaction
+    const txn = await signer.sendTransaction({
+      to: token.address,
+      data: dataWithSuffix,
+    });
 
-    // logger.info("Tx hash:", txn.hash);
+    logger.info("Tx hash:", txn.hash);
 
     // // ! End Divvi
 
-    // // const tx = await contract.transfer(props.recipient, ethers.parseUnits(props.amount, token.decimal)) // cUSD has 18 decimals
-    // // await tx.wait() // Wait for transaction to be mined
-    // logger.info(`Transaction successful: ${txn.hash}`)
+    logger.info(`Transaction successful: ${txn.hash}`)
 
-    // try {
-    //   await divvi.submitReferral({
-    //     txHash: txn.hash as `0x${string}`,
-    //     chainId: chain.chainId
-    //   });
-    //   logger.info("Referral submitted successfully");
-    // } catch (referralError) {
-    //   logger.error("Referral submission error:", referralError);
-    // }
+    try {
+      await divvi.submitReferral({
+        txHash: txn.hash as `0x${string}`,
+        chainId: chain.chainId
+      });
+      logger.info("Referral submitted successfully");
+    } catch (referralError) {
+      logger.error("Referral submission error:", referralError);
+    }
 
-    // return JSON.stringify(txn.hash)
-    return JSON.stringify(props)
+    return JSON.stringify(txn.hash)
   }
 
 
