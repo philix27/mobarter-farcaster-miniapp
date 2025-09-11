d3eed0fb68eae3a0f8f14afd098f5b869f9bb1f4
philix27 <philixbob@gmail.com>
2025-09-11 08:02:31 +0100

    set testFid to undefined on prod

--- src/lib/mongodb/const.ts
+++ src/lib/mongodb/const.ts
@@ -1,1 +1,4 @@
-export const testFid = "854428"
+import { secrets } from "../secrets";
+
+export const testFid: string | undefined = secrets.NODE_ENV === "development" ? "854428" : undefined
+// export const testFid :string| undefined= "854428"
