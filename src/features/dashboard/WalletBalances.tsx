1309f489b763d291e12e2603a0c917cf04daa309
philix27 <philixbob@gmail.com>
2025-10-05 16:29:33 +0100

    Adjust hieight of balance card

--- src/features/dashboard/WalletBalances.tsx
+++ src/features/dashboard/WalletBalances.tsx
@@ -8,7 +8,7 @@ export function WalletBalance() {
         <div className='w-full flex md:flex-row flex-col justify-around md:space-x-4 space-y-2 md:space-y-0 mb-5 md:mb-10'>
             {cardSections.map((card, i) => (
                 <div key={i} className='bg-card rounded-lg p-2 w-full border-muted border'>
-                    <div className='flex w-full items-center justify-between mb-4'>
+                    <div className='flex w-full items-center justify-between md:mb-4 mb-2'>
                         <p className='text-[12px] font-bold'>{card.title}</p>
                         <p className='text-[18px] font-thin'>{card.amount}</p>
                     </div>
