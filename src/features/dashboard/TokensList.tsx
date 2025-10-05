bdac93c8a7c609b557faacf777b165c5f51d767f
philix27 <philixbob@gmail.com>
2025-10-05 16:28:10 +0100

    setup mobile-only quick actions

--- src/features/dashboard/TokensList.tsx
+++ src/features/dashboard/TokensList.tsx
@@ -7,8 +7,8 @@ export function TokensSection() {
         <div className='w-full flex flex-col space-y-2'>
             {payTokens.map((val, i) => (
                 <div key={i} className='bg-card rounded-md p-2 flex w-full items-center justify-between'>
-                    <div className='flex items-center mr-2'>
-                        <img src={val.token.logo} alt={val.token.symbol} className='md:size-[40px] size-[20px] inline mx-2' />
+                    <div className='flex items-center mr-3 '>
+                        <img src={val.token.logo} alt={val.token.symbol} className='md:size-[35px] size-[20px] inline mx-2' />
                         <p><span className='text-[13px] font-semibold'> {val.token.symbol} </span> <span className='text-[10px] text-muted'>{val.chain.name.toUpperCase()}</span> </p>
                     </div>
                     <TokenBalanceDisplay tokenAddress={val.token.address as `0x${string}`} chainId={val.chain.chainId} />
--- src/features/dashboard/dashboard.tsx
+++ src/features/dashboard/dashboard.tsx
@@ -2,12 +2,19 @@ import React from 'react'
 import DesktopWrapper from '../desktop/Wrapper';
 import { WalletBalance } from './WalletBalances';
 import { TokensSection } from './TokensList';
+import { IconType } from 'react-icons';
+import { BsPerson, BsPhone } from 'react-icons/bs';
+import { MdSavings, } from 'react-icons/md';
+import { FaDollarSign } from 'react-icons/fa6';
+import { RiExchange2Fill } from 'react-icons/ri';
+import { useRouter } from 'next/router';
 
 export default function DashboardPage() {
     return (
         <DesktopWrapper>
             <div className='w-full  h-full flex flex-col items-center'>
                 <WalletBalance />
+                <QuickActions />
                 <TokensSection />
             </div>
         </DesktopWrapper>
@@ -15,4 +22,34 @@ export default function DashboardPage() {
 }
 
 
+export function QuickActions() {
+    const router = useRouter()
+    const actions: { Icon: IconType; title: string; href: string }[] = [
+        { title: "TopUp", href: "/bills", Icon: BsPhone, },
+        { title: "Bills", href: "/bills", Icon: FaDollarSign, },
+        { title: "Exchange", href: "/exchange", Icon: RiExchange2Fill, },
+        { title: "Savings", href: "/savings", Icon: MdSavings, },
+        { title: "Settings", href: "/profile", Icon: BsPerson, },
+    ]
 
+    return (
+        <div className='flex items-center justify-around w-full md:mb-10 mb-5 md:hidden'>
+            {actions.map((item, i) => {
+                const Icon = item.Icon;
+                return (
+                    <div key={i} className='flex flex-col items-center justify-center'
+                        onClick={() => {
+                            void router.push(item.href )
+                        }}
+                    >
+                        <div className='bg-primary p-3 rounded-md mb-1'>
+                            <Icon className='text-white' />
+                        </div>
+
+                        <p className='text-[10.4px] text-muted'>{item.title}</p>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
